1febb0826e9ea825619b74002bd3688159e89893
Connor <connor@zero.nyc>
2020-05-11 20:29:54 -0400

    mobile

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import nlp from 'compromise'
+import { useSpring, animated as a } from 'react-spring'
 const ReactMarkdown = require('react-markdown')
 
-const CorpusContext = React.createContext()
-
-let currentWord = -1
+const GlobalState = React.createContext()
+const GlobalDispatch = React.createContext()
 
 let Line = (props) => {
-  const corpusContext = React.useContext(CorpusContext)
-  let words
+  const dump = nlp(props.children)
+  let words = dump.words().map(word => ({
+    text: word.text ? word.text().trim() : "",
+    id: word.list[0] ? word.list[0].start : "",
+    raw: "",
+    tags: word.json() ? word.json()[0].terms[0].tags : []
+  }))
 
-  if(!corpusContext.parseEntire) {
-    const dump = nlp(props.children)
-    words = dump.words().map(word => ({
-      text: word.text ? word.text().trim() : "",
-      id: word.list[0] ? word.list[0].start : "",
-      raw: "",
-      tags: word.json() ? word.json()[0].terms[0].tags : []
-    }))
-  }else {
-    words = nlp.tokenize(props.children).terms().map(word => ({
-      text: word.text ? word.text().trim(): "",
-      id: word.list[0] ? word.list[0].start : "",
-      raw: word.json() ? word.json() : "",
-      tags: []
-    }))
-  }
-  // console.log("words:", words)
-  if(words.world) words = []
+  if(words.world) return null
   
   return (
     <>
@@ -41,23 +29,8 @@ let Line = (props) => {
 }
 
 let Word = (props) => {
-  const corpusContext = React.useContext(CorpusContext)
-  let tagString
+  let tagString = props.tags ? props.tags.join(" ") : ""
 
-  if(!corpusContext.parseEntire) {
-    tagString = props.tags ? props.tags.join(" ") : ""
-  }else {
-    currentWord++
-    const thisWordIndex = currentWord
-    const thisWordData = corpusContext.terms[thisWordIndex]
-
-    tagString = thisWordData ? thisWordData.terms.map(term => term.tags).reduce((acc, val) => {
-      return acc.concat(val)
-    }, []).join(" ") : ""
-  }
-
-  // console.log(props.children, props.raw, corpusContext[thisWord])
-  
   return (
     <span className={"word " + tagString}>
       <span className="word__content">
@@ -71,61 +44,93 @@ let Word = (props) => {
   )
 }
 
-let Renderer = (props) => {
-  // console.log(props)
-  // const corpusData = React.useContext(CorpusContext)
-  // console.log("corpusData:", corpusData)
-  // corpusData.debug()
+let Toolbar = (props) => {
+  const dispatch = React.useContext(GlobalDispatch)
 
   return (
-    <>
-      {props.children}
-    </>
+    <div id="toolbar">
+      <div className={`icon ${props.action}`} onClick={() => {dispatch({type: props.action})}}></div>
+    </div>
   )
 }
 
-let Editor = () => {
+let Editor = (props) => {
+  const globalState = React.useContext(GlobalState)
+
+  const styles = useSpring({
+    transform: (globalState.rendering && window.innerWidth < 769) ? "translateX(-100%)" : "translateX(0%)"
+  })
+
+  return (
+    <a.div id="editor" style={styles}>
+      <Toolbar action="play" />
+      <textarea onChange={props.handleInputChange} value={localStorage.getItem("content") ? localStorage.getItem("content") : ""}></textarea>
+    </a.div>
+  )
+}
+
+let Renderer = (props) => {
+  const globalState = React.useContext(GlobalState)
+
+  const styles = useSpring({
+    transform: (!globalState.rendering && window.innerWidth < 769) ?  "translateX(100%)" : "translateX(0%)"
+  })
+
+  return (
+    <a.div id="renderer" style={styles}>
+      <Toolbar action="edit" />
+      <ReactMarkdown source={props.rawContent} rawSourcePos={true} renderers={{
+        text: Line
+      }} />
+    </a.div>
+  )
+}
+
+function reducer(state, action){
+
+  switch (action.type) {
+    case 'play':
+      return {rendering: true}
+
+    case 'edit':
+      return {rendering: false}
+
+    default: 
+      return state
+  }
+}
+
+let App = () => {
   const [rawContent, setRawContent] = React.useState(localStorage.getItem("content") ? localStorage.getItem("content") : "")
-  const [corpusData, setCorpusData] = React.useState(nlp(""))
-  const [parseEntire, setParseEntire] = React.useState(false)
+  // const [corpusData, setCorpusData] = React.useState(nlp(""))
+
+  const [appState, dispatch] = React.useReducer(reducer, {rendering: false})
   
   let handleInputChange = (e) => {
     localStorage.setItem("content", e.target.value)
     setRawContent(e.target.value)
   }
 
-  let handleParseChange = (e) => {
-    currentWord = -1
-    setParseEntire(!parseEntire)
-  }
+  // React.useEffect(() => {
+  //   // get innerText || textContent of #renderer
+  //   let renderer =  document.getElementById("renderer")
+  //   let plainText = renderer.textContent || renderer.innerText
 
-  React.useEffect(() => {
-    // get innerText || textContent of #renderer
-    let renderer =  document.getElementById("renderer")
-    let plainText = renderer.textContent || renderer.innerText
-
-    currentWord = -1
-    // console.log(plainText)
-    // set corpusData to nlp() of innerText
-    const dump = nlp(plainText)
-    console.log(dump)
-    setCorpusData(dump.terms().json())
-  }, [rawContent])
+  //   // set corpusData to nlp() of innerText
+  //   const dump = nlp(plainText)
+  //   // console.log(dump)
+  //   setCorpusData(dump.terms().json())
+  // }, [rawContent])
   
   return (
-    <>
-      <textarea onChange={handleInputChange} value={localStorage.getItem("content") ? localStorage.getItem("content") : ""}></textarea>
-      {/* <button onClick={handleParseChange}>Parse Mode: {parseEntire ? "Entire" : "Text"}</button> */}
-      <div id="renderer">
-        <CorpusContext.Provider value={{terms: corpusData, parseEntire: parseEntire}}>
-          <ReactMarkdown source={rawContent} rawSourcePos={true} renderers={{
-            text: Line,
-            root: Renderer
-          }} />
-        </CorpusContext.Provider>
-      </div>
-    </>
+    
+    <GlobalDispatch.Provider value={dispatch}>
+      <GlobalState.Provider value={appState}>
+        <Editor handleInputChange={handleInputChange} />
+        <Renderer rawContent={rawContent} />
+      </GlobalState.Provider>
+    </GlobalDispatch.Provider>
   )
 }
 
-export default Editor;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import Editor from './App';
+import './css/main.scss';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Editor />
+    <App />
   </React.StrictMode>,
   document.getElementById('app')
 );
